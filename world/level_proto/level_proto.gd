1bb70ad3e6714631c166b15f64f0103cebd735ed
T Sch <tbs.schndlr95@gmail.com>
2026-03-25 11:55:52 +0100

    [task] - implemented simple restart logic for the test level

--- world/level_proto/level_proto.gd
+++ world/level_proto/level_proto.gd
@@ -2,12 +2,14 @@ extends Node
 
 @export var mage: MageCharacter
 @export var hud: PlayerHud
+@export var restart_menu: RestartMenu
 
 @export var skeleton_minion_scene: PackedScene
 @export var skeleton_minion_patrol_points: Array[Marker3D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	mage.died.connect(_on_player_died)
 	mage.health_changed.connect(hud.update_health)
 	mage.mana_changed.connect(hud.update_mana)
 	mage.stamina_changed.connect(hud.update_stamina)
@@ -17,3 +19,10 @@ func _ready() -> void:
 	mage.casting_progressed.connect(hud.update_skill_progress)
 	
 	hud.skill_activated.connect(mage.processor.use_skill)
+	restart_menu.restart.connect(_on_restart)
+
+func _on_player_died() -> void:
+	restart_menu.visible = true
+
+func _on_restart() -> void:
+	get_tree().reload_current_scene()
