2eb1b6c3dc69f449700a37ba4eba1ff3fd6a5a9c
T Sch <tbs.schndlr95@gmail.com>
2026-03-26 13:06:12 +0100

    [task] - implemented `SkeletonMinionController`

---
+++ entities/enemies/skeleton_minion/skeleton_minion_controller.gd
@@ -0,0 +1,78 @@
+﻿class_name SkeletonMinionController extends RefCounted
+
+var _host: CharacterBody3D
+var _agent: NavigationAgent3D
+var _patrol_points: Array[Marker3D]
+
+var _patrol_point_size: int
+var _patrol_index := 0
+
+func _init(
+	host: CharacterBody3D,
+	agent: NavigationAgent3D,
+	patrol_points: Array[Marker3D],
+) -> void:
+	_host = host
+	_agent = agent
+	_patrol_points = patrol_points
+	_patrol_point_size = patrol_points.size()
+
+func update_velocity(delta: float) -> void:
+	
+	pass
+
+func move_and_slide() -> void:
+	_host.move_and_slide()
+
+func navigate_to_target(speed: float, delta: float) -> void:
+	var next_pos := _agent.get_next_path_position()
+	var direction := (next_pos - _host.global_position)
+	
+	direction.y = 0.0
+	direction = direction.normalized()
+	
+	if not direction.is_zero_approx():
+		_look_at(direction, delta)
+	
+	var desired_velocity := direction * speed
+	_agent.set_velocity(desired_velocity)
+	_host.velocity = _agent.get_velocity()
+
+func change_target(target: Vector3) -> void:
+	_agent.target_position = target
+
+func apply_friction(friction: float) -> void:
+	_host.velocity.x = move_toward(_host.velocity.x, 0, friction)
+	_host.velocity.z = move_toward(_host.velocity.z, 0, friction)
+
+func apply_friction_if_moving(friction: float) -> void:
+	if _host.velocity.length() > 0:
+		apply_friction(friction)
+
+func next_patrol_point():
+	if _patrol_index + 1 >= _patrol_point_size:
+		_patrol_index = 0
+	else:
+		_patrol_index += 1
+	
+	_use_patrol_point_as_target()
+
+func is_navigation_finished() -> bool:
+	return _agent.is_navigation_finished()
+
+func use_first_patrol_point() -> void:
+	if _patrol_point_size == 0:
+		return
+
+	_patrol_index = 0
+	_use_patrol_point_as_target()
+
+func _look_at(direction: Vector3, delta: float) -> void:
+	var target_rotation := atan2(-direction.x, -direction.z)
+	_host.rotation.y = lerp_angle(_host.rotation.y, target_rotation, delta * 10.0)
+
+func _use_patrol_point_as_target() -> void:
+	if _patrol_point_size == 0:
+		return
+	var patrol_point := _patrol_points[_patrol_index]
+	change_target(patrol_point.global_position)
