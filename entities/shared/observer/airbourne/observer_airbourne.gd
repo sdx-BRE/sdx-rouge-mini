84e98ac23af3a23393e6825107d33e4b461feca8
T Sch <tbs.schndlr95@gmail.com>
2026-03-28 08:55:15 +0100

    [task] - implemented observer and base subscriber for notify aoirbourne and landings

---
+++ entities/shared/observer/airbourne/observer_airbourne.gd
@@ -0,0 +1,34 @@
+﻿class_name ObserverAirbourne extends RefCounted
+
+var _host: CharacterBody3D
+var _was_on_floor: bool
+
+var _on_ground_subscriber: Array[SubscriberAirbourne]
+var _on_air_subscriber: Array[SubscriberAirbourne]
+
+func _init(host: CharacterBody3D) -> void:
+	_host = host
+	_was_on_floor = _host.is_on_floor()
+
+func process() -> void:
+	var is_on_floor := _host.is_on_floor()
+	
+	if is_on_floor != _was_on_floor:
+		if is_on_floor: _notify_on_ground()
+		else: 			_notify_on_air()
+		
+		_was_on_floor = is_on_floor
+
+func subscribe_ground(subscriber: SubscriberAirbourne) -> void:
+	_on_ground_subscriber.append(subscriber)
+
+func subscribe_air(subscriber: SubscriberAirbourne) -> void:
+	_on_air_subscriber.append(subscriber)
+
+func _notify_on_ground() -> void:
+	for subscriber in _on_ground_subscriber:
+		subscriber.handle()
+
+func _notify_on_air() -> void:
+	for subscriber in _on_air_subscriber:
+		subscriber.handle()
---
+++ entities/shared/observer/airbourne/subscriber_airbourne.gd
@@ -0,0 +1,4 @@
+﻿class_name SubscriberAirbourne extends RefCounted
+
+func handle() -> void:
+	push_error("[Error][ObserverAirbourneBase]: handle() must be overwritten by child implementations")
