5deb5062ff37fa8e7572b9ee41ca4f5819a4c3b8
T Sch <tbs.schndlr95@gmail.com>
2026-03-28 11:47:34 +0100

    [chore] - cleanup

--- entities/characters/mage/abilities/mage_ability_jump.gd
+++ entities/characters/mage/abilities/mage_ability_jump.gd
@@ -1,24 +1,13 @@
 class_name MageAbilityJump extends MageAbilityInstant
 
+# Todo: implement proper support for multi jumps
+# Todo: use apex threshold and gravity multiplier on consecutive jumps,
+# Todo: e.g. set multiplier to 0.8 on second and 0.65 on third jump
+
+# Todo: adjust kinetic, implement jump buffer, so pressing jump right before landing triggers next jump
+
+# Todo: when prematurely release jump button, reduce jump height
 func trigger() -> Result:
-	DbgHelper.tprint("received jump... just buffer it")
-	
 	_controller.buffer_jump()
+	
 	return Result.Abort
-	
-	# new logic
-	
-	# old
-	#if not _controller.is_on_floor():
-	#	return Result.Abort
-	#_controller.jump()
-	
-	# Todo: implement proper support for multi jumps
-	# use apex threshold and gravity multiplier on consecutive jumps,
-	# e.g. set multiplier to 0.8 on second and 0.65 on third jump
-	
-	# adjust kinetic, implement jump buffer, so pressing jump right before landing triggers next jump
-	
-	# when prematurely release jump button, reduce jump height
-	
-	return Result.Trigger
--- entities/characters/mage/mage_motor.gd
+++ entities/characters/mage/mage_motor.gd
@@ -6,11 +6,6 @@ func _init(context: MageMovementContext) -> void:
 	_ctx = context
 
 func apply_impulses(_delta: float) -> void:
-	DbgHelper.tprint("[MageMotor::apply_impulses]")
-	DbgHelper.tprint("jump_buffer_timer: ", _ctx.motion.jump_buffer_timer, " coyote_timer: ", _ctx.motion.coyote_timer)
-	print()
-	
-	
 	if _ctx.motion.jump_buffer_timer > 0 and _ctx.motion.coyote_timer > 0:
 		_ctx.host.velocity.y = _ctx.config.get_jump_impulse_velocity()
 		_ctx.motion.coyote_timer = 0
