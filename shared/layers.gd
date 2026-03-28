06e77a841f7f6541b49eb633267db9c72d198426
T Sch <tbs.schndlr95@gmail.com>
2026-03-28 12:34:05 +0100

    [chore] - code format

--- shared/layers.gd
+++ shared/layers.gd
@@ -1,12 +1,12 @@
 class_name Layers extends RefCounted
 
-const VISUAL_WORLD = 	1 << 0
-const VISUAL_ENTITY = 	1 << 1
-const VISUAL_EFFECT = 	1 << 2
+const VISUAL_WORLD := 	1 << 0
+const VISUAL_ENTITY := 	1 << 1
+const VISUAL_EFFECT := 	1 << 2
 
-const COLLISION_PLAYER_COLLISION = 	1 << 0
-const COLLISION_PLAYER_DAMAGE = 	1 << 1
-const COLLISION_ENEMY_COLLISION = 	1 << 2
-const COLLISION_ENEMY_DAMAGE = 		1 << 3
-const COLLISION_WORLD = 			1 << 4
-const COLLISION_SPELL = 			1 << 5
+const COLLISION_PLAYER_COLLISION := 	1 << 0
+const COLLISION_PLAYER_DAMAGE := 		1 << 1
+const COLLISION_ENEMY_COLLISION := 		1 << 2
+const COLLISION_ENEMY_DAMAGE := 		1 << 3
+const COLLISION_WORLD := 				1 << 4
+const COLLISION_SPELL := 				1 << 5
