a319b420886db9fdce597aeb3e5526c87d5ca878
T Sch <tbs.schndlr95@gmail.com>
2026-03-28 19:24:53 +0100

    [task] - improved firebolt with new functionality to follow targets

--- entities/characters/mage/abilities/mage_ability_firebolt.gd
+++ entities/characters/mage/abilities/mage_ability_firebolt.gd
@@ -1,11 +1,66 @@
-class_name MageAbilityFirebolt extends MageAbilitySpell.Instant
+class_name MageAbilityFirebolt extends MageAbilitySpell
 
-func execute() -> void:
+var _target: Node3D
+var _is_started := false
+
+func execute() -> ExecuteResult:
+	if _target == null:
+		return ExecuteResult.Cancel
+	
 	_context.notify_casting_end()
-	_init_at_wand_spawnpoint(_anim.scene)
+	_context.use_captured_mouse()
+	_context.hide_enemy_target_marker()
+	
+	var node := _init_at_wand_spawnpoint(_anim.scene) as Firebolt
+	var target = _target.get_target_point() if _target.has_method("get_target_point") else _target
+	
+	node.set_target(target)
+	
+	return ExecuteResult.Trigger
 
 func start() -> MageAbilityPhased.StartResult:
-	_context.request_oneshot_animation(_anim.trigger)
-	_context.notify_casting_started()
+	_context.use_visible_mouse()
+	_is_started = false
 	
-	return MageAbilityPhased.StartResult.Cast
+	return MageAbilityPhased.StartResult.HandleWithInput
+
+func update(_delta: float) -> void:
+	var cast_range := 120.0
+	var result := _context.raycast_from_mouse(cast_range, Layers.COLLISION_ENEMY_COLLISION)
+	
+	if result:
+		var enemy_position = result.collider.global_position + Vector3(0.0, 1.0, 0.0)
+		var enemy_position_2d := _context.unproject_position(enemy_position)
+		
+		_context.set_enemy_target_marker_position(enemy_position_2d)
+		_context.show_enemy_target_marker()
+		_target = result.collider
+	else:
+		_context.hide_enemy_target_marker()
+		_target = null
+
+func cancel() -> void:
+	_context.use_captured_mouse()
+	_context.hide_enemy_target_marker()
+
+func handle_input(event: InputEvent) -> HandleInputResult:
+	var is_cancel_pressed := event.is_action_pressed(MageAbilityActions.ACTION_CANCEL)
+	var is_action_pressed_again := _is_started and event.is_action(MageAbilityActions.ACTION_FIREBOLT)
+	
+	var is_canceled := is_cancel_pressed or is_action_pressed_again
+	if is_canceled:
+		return HandleInputResult.Cancel
+	
+	if event.is_action_released(MageAbilityActions.ACTION_FIREBOLT):
+		_is_started = true
+	
+	if event.is_action_pressed(MageAbilityActions.ACTION_ATTACK):
+		if _target == null:
+			return HandleInputResult.Cancel
+		
+		_context.request_oneshot_animation(_anim.trigger)
+		_context.use_captured_mouse()
+		_context.notify_casting_started()
+		return HandleInputResult.Trigger
+	
+	return HandleInputResult.Unhandled
--- entities/characters/mage/spells/firebolt/firebolt.gd
+++ entities/characters/mage/spells/firebolt/firebolt.gd
@@ -1,20 +1,44 @@
-extends Area3D
+class_name Firebolt extends Area3D
 
 @export var speed: float = 20.0
 @export var lifespan: float = 5.0
 @export var dmg: float = 15
+@export var homing_steer_speed := 1.5
+@export var homing_fov := -0.6
+
+var _target: Node3D
 
 func _ready() -> void:
 	get_tree().create_timer(lifespan).timeout.connect(queue_free)
 	area_entered.connect(_on_area_entered)
 
 func _process(delta: float) -> void:
-	var forward = -global_transform.basis.z
+	if _target == null:
+		push_warning("[Firebolt].process() - no target set!")
+		queue_free()
+		return
+	
+	var forward := -global_transform.basis.z
+	
+	_smooth_look_at(_target.global_position, forward, delta)
 	global_position += forward * speed * delta
 
+func _smooth_look_at(target_pos: Vector3, forward: Vector3, delta: float) -> void:
+	var direction := (target_pos - global_position).normalized()
+	
+	var dot := forward.dot(direction)
+	if dot > homing_fov:
+		var target_transform := global_transform.looking_at(target_pos, Vector3.UP)
+		global_transform.basis = global_transform.basis \
+			.slerp(target_transform.basis, delta * homing_steer_speed) \
+			.orthonormalized()
+
 func _on_area_entered(body: Node3D) -> void:
-	var hitbox = body as DamageHitbox
+	var hitbox := body as DamageHitbox
 	if hitbox != null:
 		hitbox.take_dmg(dmg)
 	
 	queue_free()
+
+func set_target(target: Node3D) -> void:
+	_target = target
