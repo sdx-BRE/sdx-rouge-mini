e00406b4e653110910d4f7c01663c3f87c74dedd
T Sch <tbs.schndlr95@gmail.com>
2026-03-29 21:11:59 +0200

    [task] - finished mage sprint ability

--- entities/characters/mage/abilities/mage_ability_sprint.gd
+++ entities/characters/mage/abilities/mage_ability_sprint.gd
@@ -1,3 +1,9 @@
-class_name MageAbilitySprint extends MageAbilityBase
+class_name MageAbilitySprint extends MageAbilityChanneled
 
+func end() -> void:
+	_controller.use_normal_speed()
 
+func tick(_delta: float) -> TickResult:
+	_controller.use_sprinting_speed()
+	
+	return TickResult.Consume
