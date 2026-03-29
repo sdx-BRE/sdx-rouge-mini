b097172040a1cae0f735cfa7df0b160443b82307
T Sch <tbs.schndlr95@gmail.com>
2026-03-29 21:11:05 +0200

    [chore] - mage ability default data adjustments

--- entities/characters/mage/mage_data.gd
+++ entities/characters/mage/mage_data.gd
@@ -11,15 +11,15 @@ class_name MageData extends Resource
 
 @export_group("Movement")
 @export var speed_normal := 5.0
-@export var speed_sprinting := 5.0
+@export var speed_sprinting := 15.0
 @export var dash_power := 20.0
 @export var dash_decay := 8.0
 
 @export_group("Jump")
 @export var jump_height := 5.0
-@export var time_to_peak := 0.35
-@export var time_to_descent := 0.25
-@export var apex_threshold := 6.0
+@export var time_to_peak := 0.5
+@export var time_to_descent := 0.6
+@export var apex_threshold := 3.0
 @export var apex_gravity_multiplier := 1.0
 @export var coyote_time := 0.1 # usually between 0.08 – 0.15 second
 @export var jump_buffer_time := 0.15
