8065aa1366adb45a9589bac0a78c862e58441534
T Sch <tbs.schndlr95@gmail.com>
2026-03-31 12:01:47 +0200

    [fix] - fixed SpellAnimationData.CALL_METHOD_TRACK_NAME to reflect mages renamed method

--- entities/characters/mage/spells/spell_animation_data.gd
+++ entities/characters/mage/spells/spell_animation_data.gd
@@ -2,7 +2,7 @@ class_name SpellAnimationData extends MageAnimationData
 
 @export var animation_name: StringName
 
-const CALL_METHOD_TRACK_NAME := "execute_spell"
+const CALL_METHOD_TRACK_NAME := &"execute_buffered_ability"
 
 func try_get_call_method_track_time(player: AnimationPlayer):
 	return AnimationUtil.try_get_call_method_track_time(player, animation_name, CALL_METHOD_TRACK_NAME)
