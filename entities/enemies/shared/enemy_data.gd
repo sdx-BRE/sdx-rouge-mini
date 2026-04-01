4f47dcf914b7e56c14a20d63b055ccebc18ff6ee
T Sch <tbs.schndlr95@gmail.com>
2026-04-01 15:29:59 +0200

    [task] - add stamina to enemy data

--- entities/enemies/shared/enemy_data.gd
+++ entities/enemies/shared/enemy_data.gd
@@ -1,6 +1,8 @@
 class_name EnemyData extends Resource
 
 @export var max_health: float = 100.0
+@export var max_mana: float = 0.0
+@export var max_stamina: float = 50.0
 @export var walking_speed: float = 2.5
 @export var running_speed: float = 5.0
 @export var wait_time: float = 2.5
