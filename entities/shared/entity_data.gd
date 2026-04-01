da04a08675030a8e4886fddb538cec0e5be845cc
T Sch <tbs.schndlr95@gmail.com>
2026-04-01 15:28:46 +0200

    [task] - create entity stats and entity data resources

---
+++ entities/shared/entity_data.gd
@@ -0,0 +1,11 @@
+﻿class_name EntityData extends Resource
+
+@export_group("Base stats")
+@export var health := 100.0
+@export var mana := 80.0
+@export var stamina := 100.0
+
+@export_group("Stat regeneration")
+@export var mana_regeneration := 1.618
+@export var stamina_regeneration := 3.1415
+
