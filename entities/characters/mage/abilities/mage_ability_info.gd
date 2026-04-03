c3f15e318650c461756fc924c75853ebafa2568e
T Sch <tbs.schndlr95@gmail.com>
2026-04-03 17:59:53 +0200

    [cleanup] - removed all files of old mage ability system (which now lives in entities/shared/ability_system)

--- entities/characters/mage/abilities/mage_ability_info.gd
+++
@@ -1,8 +0,0 @@
-class_name MageAbilityInfo extends RefCounted
-
-var ability: MageAbilityBase
-var actions: Array[StringName]
-
-func _init(p_ability: MageAbilityBase, p_actions: Array[StringName]) -> void:
-	ability = p_ability
-	actions = p_actions
--- entities/characters/mage/abilities/mage_ability_sprint.gd
+++
@@ -1,9 +0,0 @@
-class_name MageAbilitySprint extends MageAbilityChanneled
-
-func end() -> void:
-	_controller.use_normal_speed()
-
-func tick(_delta: float) -> TickResult:
-	_controller.use_sprinting_speed()
-	
-	return TickResult.Consume
