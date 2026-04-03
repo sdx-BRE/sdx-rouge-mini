b27d646f94d6a93d9b7c3986f31f2f3f29477bd8
T Sch <tbs.schndlr95@gmail.com>
2026-04-03 17:06:30 +0200

    [chore] - increased dash power

--- entities/characters/shared/ability_system/data/skill/character_ability_dash.gd
+++ entities/characters/shared/ability_system/data/skill/character_ability_dash.gd
@@ -1,7 +1,7 @@
 ﻿@tool
 class_name CharacterAbilityDash extends CharacterAbilityData
 
-@export var dash_power: float = 10.0
+@export var dash_power: float = 60.0
 @export var anim_trigger: StringName
 
 func to_ability(
