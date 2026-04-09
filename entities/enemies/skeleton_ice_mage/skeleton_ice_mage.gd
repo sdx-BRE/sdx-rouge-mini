a35a54d2279017c58d0eff2d690b1e8d2bafd615
T Sch <tbs.schndlr95@gmail.com>
2026-04-09 16:03:46 +0200

    [refactoring] - replace skeleton ice mage implementation with new one which uses the base skeleton enemy scene

--- entities/enemies/skeleton_ice_mage/skeleton_ice_mage.gd
+++ entities/enemies/skeleton_ice_mage/skeleton_ice_mage.gd
@@ -1,99 +1,12 @@
-class_name SkeletonIceMage extends CharacterBody3D
-
-const ATTACK_RANGE := 15.0
-
-signal died()
-
-@export_group("Skeleton properties")
-@export var data: EnemyData
-
-@export_group("Patrol settings")
-@export var wait_time: float = 5.0
-@export var patrol_points: Array[Marker3D]
+class_name SkeletonIceMage extends BaseSkeletonEnemy
 
 @export_group("Abilities")
 @export var frost_bolt: EnemyCastProjectileAbility
 @export var ground_aoe: EnemyCastAbilityArea
-@export var dev_ability: EnemyInstantAbility
 
-@export_group("Animation - General")
-@export var anim_tree: AnimationTree
-@export var path_playback_full_body: String
-@export var path_locomotion_blend: String
-@export var path_locomotion_timescale: String
-
-@export_group("Animation - State names")
-@export var state_death: StringName
-
-@export_group("Animation - Oneshot")
-@export var oneshot_hit_weak: StringName
-@export var oneshot_hit_strong: StringName
-@export var oneshot_spawn_air: StringName
-@export var oneshot_spawn_ground: StringName
-@export var oneshot_shoot: StringName
-@export var oneshot_raise: StringName
-
-@export_group("Animation - Thresholds")
-@export var threshold_hit_strong: float = 12.5
-
-@export_group("Field of view")
-@export var fov_angle: float = 70
-
-@onready var pivot := $Pivot
-@onready var agent: NavigationAgent3D = $NavigationAgent3D
-@onready var ui: EnemyUI = $EnemyViewport
-@onready var fov: Area3D = $Fov
-@onready var target_point := $TargetPoint
 @onready var staff_spawn_point := $Pivot/Rig_Medium/Skeleton3D/BoneAttachment3D/staff2/StaffSpawnPoint
 
-var _anim: SkeletonIceMageAnimator
-var _stats: EntityStats
-var _processor: EntityProcessor
-var _target_handler: AiTargetHandler
 var _ability_system: EnemyAbilitySystem
-
-func _ready() -> void:
-	SkeletonIceMageBootstrapper.bootstrap(self)
-
-func on_die() -> void:
-	if not anim_tree.animation_finished.is_connected(on_death_anim_finished):
-		anim_tree.animation_finished.connect(on_death_anim_finished)
-	
-	_anim.die()
-	set_physics_process(false)
-	set_process(false)
-
-func on_death_anim_finished(anim_name: StringName) -> void:
-	if anim_name == state_death:
-		anim_tree.animation_finished.disconnect(on_death_anim_finished)
-		await get_tree().create_timer(1.0).timeout
-		died.emit()
-		queue_free()
-
-func _physics_process(delta: float) -> void:
-	_processor.physics_process(delta)
-
-func _process(delta: float) -> void:
-	_processor.process(delta)
 
 func execute_cast() -> void:
 	_ability_system.execute_cast()
-
-func take_dmg(value: float) -> void:
-	_stats.take_dmg(value)
-	if not _stats.is_alive():
-		return
-	
-	if value >= threshold_hit_strong:
-		_anim.hit_strong()
-	else:
-		_anim.hit_weak()
-
-func get_target_point() -> Marker3D:
-	return target_point
-
-func _on_fov_entered(body: Node3D):
-	_target_handler.add_target(body)
-
-func _on_fov_exited(body: Node3D):
-	_target_handler.remove_target(body)
--- entities/enemies/skeleton_ice_mage/skeleton_ice_mage_bootstrapper.gd
+++ entities/enemies/skeleton_ice_mage/skeleton_ice_mage_bootstrapper.gd
@@ -1,59 +1,32 @@
-class_name SkeletonIceMageBootstrapper extends RefCounted
+﻿class_name SkeletonIceMageBootstrapper extends BaseSkeletonEnemyBootstrapper
 
-static func bootstrap(entity: SkeletonIceMage) -> void:
-	var anim := _create_enemy_animator(entity)
-	_bootstrap_props(entity, anim)
-	_bootstrap_ability_system(entity, anim)
-	_bootstrap_processor(entity)
-	_wire_signals(entity)
+var _mage: SkeletonIceMage
 
-static func _bootstrap_props(entity: SkeletonIceMage, anim: EnemyAnimator) -> void:
-	var fov_threshold := cos(deg_to_rad(entity.fov_angle / 2.0))
-	
-	entity._stats = EntityStats.from_enemy_data(entity.data)
-	entity._target_handler = AiTargetHandler.new(entity, entity.ATTACK_RANGE, fov_threshold)
-	
-	var anim_params := SkeletonIceMageAnimationParams.from_entity(entity)
-	var animator := EnemyAnimator.new(entity.anim_tree)
-	animator.add_playback_from_param(EnemyAnimator.StatePlayback.FullBody, entity.path_playback_full_body)
-	entity._anim = SkeletonIceMageAnimator.new(anim, anim_params)
+func setup(mage: SkeletonIceMage) -> void:
+	_mage = mage
 
-static func _bootstrap_processor(entity: SkeletonIceMage) -> void:
-	# Todo: Replace hard coded config
-	var config := EnemyMovementConfig.new()
-	
-	var motion := EnemyMovementMotion.create()
-	var context := EnemyMovementContext.new(entity, entity.agent, config, motion)
-	var kinematics := EnemyKinematics.new(context)
-	
-	var state_machine := create_state_machine(entity, context)
-	
-	entity._processor = EntityProcessor.new(entity.get_viewport())
-	entity._processor.add_process_handler(EnemyAbilitySystemHandler.new(entity._ability_system))
-	entity._processor.add_process_handler(EnemyStateMachineHandler.new(state_machine))
-	
-	entity._processor.add_physics_handler(EnemyVelocityHandler.new(kinematics))
-	entity._processor.add_physics_handler(SkeletonIceMageLocomotionHandler.new(
-		kinematics,
-		entity._anim, 
-		entity.data.walking_speed, 
-		entity.data.running_speed,
-	))
-	entity._processor.add_physics_handler(EnemyCollisionsHandler.new(kinematics))
+func _boot_processor() -> void:
+	_bootstrap_ability_system()
+	super()
 
-static func _bootstrap_ability_system(entity: SkeletonIceMage, anim: EnemyAnimator) -> void:
+func _create_attack_context() -> StateContextAttack:
+	return SkeletonIceMageAttack.new(_mage._ability_system)
+
+func _boot_processor_process_handler() -> void:
+	_mage._processor.add_process_handler(EnemyAbilitySystemHandler.new(_mage._ability_system))
+
+func _bootstrap_ability_system() -> void:
 	var registry := EnemyAbilityRegistry.new()
-	registry.add_ability(EnemyAbilityId.FROST_BOLT, entity.frost_bolt)
-	registry.add_ability(EnemyAbilityId.SIMPLE_DEV_AOE, entity.ground_aoe)
-	registry.add_ability(EnemyAbilityId.DEV, entity.dev_ability)
+	registry.add_ability(EnemyAbilityId.FROST_BOLT, _mage.frost_bolt)
+	registry.add_ability(EnemyAbilityId.SIMPLE_DEV_AOE, _mage.ground_aoe)
 	
 	var cast_context := EnemyAbilityContextCast.create(
-		entity._stats,
-		entity,
-		entity.pivot,
-		anim,
-		entity._target_handler,
-		entity.staff_spawn_point
+		_mage._stats,
+		_mage,
+		_mage.pivot,
+		_mage._anim.get_animator(),
+		_mage._target_handler,
+		_mage.staff_spawn_point
 	)
 	
 	var cooldown_manager := CooldownManager.new()
@@ -62,44 +35,4 @@ static func _bootstrap_ability_system(entity: SkeletonIceMage, anim: EnemyAnimat
 		EnemyAbilityHandlerInstant.new(),
 	)
 	
-	entity._ability_system =  EnemyAbilitySystem.new(registry, resolver, cooldown_manager)
-
-static func create_state_machine(
-	entity: SkeletonIceMage,
-	movement_context: EnemyMovementContext,
-) -> EnemyStateMachine:
-	var aggro_rule := StateTransitionRule.new(
-		StateTransition.AGGRO,
-		func() -> bool: return entity._target_handler.update_and_has_target()
-	)
-	
-	var controller := EnemyController.new(movement_context, entity.patrol_points)
-	var blackboard := EnemyBlackboard.from_data(entity.data)
-	
-	var attack_context := SkeletonIceMageAttack.new(entity._ability_system)
-	var context := StateContext.new(entity._target_handler, controller, blackboard, entity.data, entity._stats, attack_context)
-	
-	var waiting_state := WaitingState.new(context)
-	var walking_state := WalkingState.new(context)
-	var aggro_state := AggroState.new(context)
-	
-	var states: Dictionary[int, StateBase] = {}
-	states.set(StateTransition.AGGRO, aggro_state)
-	states.set(StateTransition.WALKING, walking_state)
-	states.set(StateTransition.WAITING, waiting_state)
-	
-	return EnemyStateMachine.new(walking_state, states, context, [aggro_rule])
-
-static func _wire_signals(entity: SkeletonIceMage) -> void:
-	if entity.ui is EnemyUI:
-		entity._stats.health_changed.connect(entity.ui.update_health)
-	entity._stats.health_reached_zero.connect(entity.on_die)
-	
-	entity.fov.area_entered.connect(entity._on_fov_entered)
-	entity.fov.area_exited.connect(entity._on_fov_exited)
-
-static func _create_enemy_animator(entity: SkeletonIceMage) -> EnemyAnimator:
-	var anim := EnemyAnimator.new(entity.anim_tree)
-	anim.add_playback_from_param(EnemyAnimator.StatePlayback.FullBody, entity.path_playback_full_body)
-	
-	return anim
+	_mage._ability_system =  EnemyAbilitySystem.new(registry, resolver, cooldown_manager)
