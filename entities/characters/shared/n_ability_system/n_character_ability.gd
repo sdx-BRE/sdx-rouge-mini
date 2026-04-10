5fe6f45b1b9b1711b6593da45c8d7485e1d7b9d9
T Sch <tbs.schndlr95@gmail.com>
2026-04-10 12:17:04 +0200

    [refactoring] - migrated character ability and character ability system code to new system

--- entities/characters/shared/n_ability_system/n_character_ability.gd
+++ entities/characters/shared/n_ability_system/n_character_ability.gd
@@ -1,9 +1,18 @@
 ﻿class_name NCharacterAbility extends RefCounted
 
 var _data: NCharacterAbilityData
+var _stats: EntityStats
 
-func _init(data: NCharacterAbilityData) -> void:
+func _init(data: NCharacterAbilityData, stats: EntityStats) -> void:
 	_data = data
+	_stats = stats
+
+func has_resources() -> bool:
+	return _stats.has_mana(_data.cost.mana) and _stats.has_stamina(_data.cost.stamina)
+
+func use_resources() -> void:
+	_stats.use_mana(_data.cost.mana)
+	_stats.use_stamina(_data.cost.stamina)
 
 func get_input() -> StringName:
 	return _data.input
--- entities/characters/shared/n_ability_system/n_character_ability_system.gd
+++ entities/characters/shared/n_ability_system/n_character_ability_system.gd
@@ -14,21 +14,55 @@ func _init(
 	_cooldown_manager = cooldown_manager
 
 func handle_input(event: InputEvent) -> bool:
-	#print("handle ")
+	if event.is_echo():
+		return false
+	
+	if _manager.is_handling_active_ability(event):
+		return true
+	
+	var actions := _registry.get_actions()
+	for action in actions:
+		if event.is_action(action):
+			var state := NCharacterAbilitySystem.TriggerState.Press if event.is_pressed() else NCharacterAbilitySystem.TriggerState.Release
+			
+			_try_activate_ability(actions[action], state)
+			return true
+	
 	return false
 
 func execute_buffered_ability() -> void:
-	#print("NCharacterSystem.execute_buffered_ability()")
-	pass
+	_manager.execute_buffered_ability()
 
 func on_ability_triggered(id: int) -> void:
-	#print("NCharacterSystem.on_ability_triggered()")
-	pass
+	var ability := _registry.get_ability(id)
+	if ability == null:
+		return
+	
+	ability.use_resources()
 
 func has_resources(id: int) -> bool:
-	#print("NCharacterSystem.has_resources()")
-	return false
+	var ability := _registry.get_ability(id)
+	if ability == null:
+		return false
+	
+	return ability.has_resources()
 
 func tick(delta: float) -> void:
-	#print("NCharacterSystem.tick()")
-	pass
+	_manager.tick(delta)
+	_cooldown_manager.tick(delta)
+
+func _try_activate_ability(
+	id: int,
+	state: TriggerState,
+) -> void:
+	var ability := _registry.get_ability(id)
+	
+	if ability == null:
+		return
+	
+	_manager.try_activate(ability, state)
+
+enum TriggerState {
+	Press,
+	Release,
+}
