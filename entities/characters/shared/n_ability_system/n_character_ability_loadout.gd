0aee6d7dd6b356505cbf9888f88f2268079c2679
T Sch <tbs.schndlr95@gmail.com>
2026-04-10 13:21:56 +0200

    [dev]

--- entities/characters/shared/n_ability_system/n_character_ability_loadout.gd
+++ entities/characters/shared/n_ability_system/n_character_ability_loadout.gd
@@ -1,13 +1,16 @@
 ﻿class_name NCharacterAbilityLoadout extends RefCounted
 
 var _registry: NCharacterAbilityRegistry
+var _stats: EntityStats
 
 func _init(
-	registry: NCharacterAbilityRegistry
+	registry: NCharacterAbilityRegistry,
+	stats: EntityStats,
 ) -> void:
 	_registry = registry
+	_stats = stats
 
 func grant_ability(data: NCharacterAbilityData) -> void:
-	var ability := data.to_ability(null, null) # Todo: fixme
+	var ability := data.to_ability(_stats, null) # Todo: fixme
 	_registry.add(data.id, ability)
 
