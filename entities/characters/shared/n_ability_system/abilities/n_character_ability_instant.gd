60b5f06b5cb8bf16099ece5ad2549fcb14712c19
T Sch <tbs.schndlr95@gmail.com>
2026-04-10 13:17:15 +0200

    [refactoring] - finished migrationg instant handler

---
+++ entities/characters/shared/n_ability_system/abilities/n_character_ability_instant.gd
@@ -0,0 +1,15 @@
+﻿class_name NCharacterAbilityInstant extends RefCounted
+
+var base: NCharacterAbility
+
+func _init(ability: NCharacterAbility) -> void:
+	base = ability
+
+func trigger(state: NCharacterAbilitySystem.TriggerState) -> Result:
+	push_error("[Error][CharacterInstantAbility]: trigger() must be overwritten by child implementations - state: ", state)
+	return Result.NoCost
+
+enum Result {
+	Consume,
+	NoCost,
+}
--- entities/characters/shared/n_ability_system/handler/n_character_ability_handler_instant.gd
+++ entities/characters/shared/n_ability_system/handler/n_character_ability_handler_instant.gd
@@ -1,2 +1,14 @@
 ﻿class_name NCharacterAbilityHandlerInstant extends NCharacterAbilityHandler
 
+var _ability: NCharacterAbilityInstant
+
+func setup(ability: NCharacterAbility) -> void:
+	_ability = NCharacterAbilityInstant.new(ability)
+
+func try_activate(state: NCharacterAbilitySystem.TriggerState) -> void:
+	if not _ability.has_resources() or _cooldown_manager.has_character_cooldown(_ability._data):
+		return
+	
+	if _ability.trigger(state) == NCharacterAbilityInstant.Result.Consume:
+		_ability.use_resources()
+		_cooldown_manager.start_character_cooldown(_ability._data)
