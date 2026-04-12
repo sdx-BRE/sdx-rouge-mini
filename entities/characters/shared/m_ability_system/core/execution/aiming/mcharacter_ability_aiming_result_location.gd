4503cb37e9504f475bb4ed9cf7699504dde7ea31
T Sch <tbs.schndlr95@gmail.com>
2026-04-12 13:52:09 +0200

    [task] - aiming targets now handle both, aoe and projectiles, properly

--- entities/characters/shared/m_ability_system/core/execution/aiming/mcharacter_ability_aiming_result_location.gd
+++ entities/characters/shared/m_ability_system/core/execution/aiming/mcharacter_ability_aiming_result_location.gd
@@ -7,3 +7,14 @@ func _init(pos: Vector3) -> void:
 
 func set_aoe_position(aoe: BaseAoe) -> void:
 	aoe.global_position = position
+
+func set_projectile_target(projectile: BaseProjectile) -> void:
+	var target_node := Node3D.new()
+	
+	projectile.add_child(target_node)
+	projectile.tree_exited.connect(target_node.queue_free)
+	
+	target_node.top_level = true
+	target_node.global_position = position
+	
+	projectile._target = target_node
--- entities/characters/shared/m_ability_system/core/execution/aiming/mcharacter_ability_aiming_result_mouse_target.gd
+++ entities/characters/shared/m_ability_system/core/execution/aiming/mcharacter_ability_aiming_result_mouse_target.gd
@@ -1,4 +1,4 @@
-﻿class_name McharacterAbilityAimingResultMouseTarget extends McharacterAbilityAimingResult
+class_name McharacterAbilityAimingResultMouseTarget extends McharacterAbilityAimingResult
 
 var target: Node3D
 
@@ -7,3 +7,7 @@ func _init(target_node: Node3D) -> void:
 
 func set_projectile_target(projectile: BaseProjectile) -> void:
 	projectile._target = target
+
+func set_aoe_position(aoe: BaseAoe) -> void:
+	if target != null:
+		aoe.global_position = target.global_position
