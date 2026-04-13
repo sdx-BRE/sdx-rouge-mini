70f2d2dc2f1b7910e9b163fdf76ce39b1ff9913f
T Sch <tbs.schndlr95@gmail.com>
2026-04-13 16:12:34 +0200

    [task] - extend ability cost, add type information

--- entities/shared/abilities/ability_cost.gd
+++ entities/shared/abilities/ability_cost.gd
@@ -1,4 +1,11 @@
 ﻿class_name AbilityCost extends Resource
 
+enum Type {
+	Instant,
+	Tick,
+	External,
+}
+
+@export var type: Type = Type.External
 @export var mana: float = 0.0
 @export var stamina: float = 0.0
