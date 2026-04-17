333af04d643b692aeb6055ac45e9f3227ea1452b
T Sch <tbs.schndlr95@gmail.com>
2026-04-17 13:29:14 +0200

    [task] - implement continuous_ability.gd base class

---
+++ entities/abilities/continuous/continuous_ability.gd
@@ -0,0 +1,34 @@
+class_name ContinuousAbility extends AbilityEntity
+
+var _damage: float
+
+var _targets: Array[Node3D] = []
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
+
+func setup_enemy_ability(_ability: EnemyCastAbility, _context: EnemyAbilityContextCast) -> void:
+	collision_layer = Layers.COLLISION_ENEMY_SPELL
+	collision_mask = Layers.COLLISION_PLAYER_DAMAGE
+
+func setup_character_ability(data: CharacterAbilityDelivery, _context: CharacterAbilityExecuteContext) -> void:
+	collision_layer = Layers.COLLISION_PLAYER_SPELL
+	collision_mask = Layers.COLLISION_ENEMY_DAMAGE
+	
+	var continuous_data := data as CharacterAbilityDeliveryContinuous
+	if continuous_data == null:
+		return
+	
+	_damage = continuous_data.damage
+
+func tick_damage() -> void:
+	for target in _targets:
+		if target.has_method("take_dmg"):
+			target.take_dmg(_damage)
+
+func _on_area_entered(body: Node3D) -> void:
+	_targets.append(body)
+
+func _on_area_exited(body: Node3D) -> void:
+	_targets.erase(body)
