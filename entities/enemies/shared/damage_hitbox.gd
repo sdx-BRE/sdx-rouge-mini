b50676025910ab1fef3663b00427b1f662791e2c
T Sch <tbs.schndlr95@gmail.com>
2026-04-17 16:08:18 +0200

    [task] - `DamageHitbox` sends damage information across the system

--- entities/enemies/shared/damage_hitbox.gd
+++ entities/enemies/shared/damage_hitbox.gd
@@ -3,6 +3,8 @@ class_name DamageHitbox extends Area3D
 @export var entity: CharacterBody3D
 
 func take_dmg(value: float) -> void:
+	DamageGateway.display_damage.emit(value, global_position)
+	
 	if entity != null and entity.has_method("take_dmg"):
 		entity.take_dmg(value)
 	elif get_parent().has_method("take_dmg"):
