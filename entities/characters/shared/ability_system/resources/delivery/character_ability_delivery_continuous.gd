222e1c55c631e7d4e65dec5d4309e2003a839ef7
T Sch <tbs.schndlr95@gmail.com>
2026-04-17 10:24:50 +0200

    [fix] - add damage prop to DeliveryContinuous

--- entities/characters/shared/ability_system/resources/delivery/character_ability_delivery_continuous.gd
+++ entities/characters/shared/ability_system/resources/delivery/character_ability_delivery_continuous.gd
@@ -1,6 +1,7 @@
 ﻿class_name CharacterAbilityDeliveryContinuous extends CharacterAbilityDelivery
 
 @export var scene: PackedScene
+@export var damage := 10.0
 
 func create_handler(
 	context: CharacterAbilityExecuteContext,
