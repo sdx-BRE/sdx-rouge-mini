2f674aaebc0e5132981c6819a3af92a1816bd544
T Sch <tbs.schndlr95@gmail.com>
2026-04-21 16:36:34 +0200

    [task] - scaffold skeleton boss enemy

---
+++ entities/enemies/skeleton_boss/skeleton_boss_attack.gd
@@ -0,0 +1,7 @@
+﻿class_name SkeletonBossAttack extends StateContextAttack
+
+func try_attack(context: StateContext) -> void:
+	if context.can_attack():
+		context.start_attack_cooldown()
+		
+		print("buds")
--- entities/enemies/skeleton_boss/skeleton_boss_bootstrapper.gd
+++ entities/enemies/skeleton_boss/skeleton_boss_bootstrapper.gd
@@ -1,3 +1,4 @@
-﻿class_name SkeletonBossBootstrapper extends BaseSkeletonEnemyBootstrapper
+class_name SkeletonBossBootstrapper extends BaseSkeletonEnemyBootstrapper
 
-
+func _create_attack_context() -> StateContextAttack:
+	return SkeletonBossAttack.new()
