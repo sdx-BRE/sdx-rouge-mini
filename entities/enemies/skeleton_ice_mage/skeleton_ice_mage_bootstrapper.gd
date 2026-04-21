faa2716dca1156346b979cb9119a867bc4611b05
T Sch <tbs.schndlr95@gmail.com>
2026-04-21 14:14:39 +0200

    [refactoring] - simplify SkeletonIceMage and bootstrapper

--- entities/enemies/skeleton_ice_mage/skeleton_ice_mage_bootstrapper.gd
+++ entities/enemies/skeleton_ice_mage/skeleton_ice_mage_bootstrapper.gd
@@ -5,42 +5,13 @@ var _mage: SkeletonIceMage
 func setup(mage: SkeletonIceMage) -> void:
 	_mage = mage
 
-func _boot_processor() -> void:
-	_bootstrap_ability_system()
-	super()
-
-func _create_attack_context() -> StateContextAttack:
-	return SkeletonIceMageAttack.new(_mage._ability_system)
-
-func _boot_processor_process_handler() -> void:
-	_mage._processor.add_process_handler(EnemyAbilitySystemHandler.new(_mage._ability_system))
-
-func _bootstrap_ability_system() -> void:
-	var registry := AbilityRegistry.new()
-	var cooldown_manager := CooldownManager.new()
-	
-	registry.register(_mage.dev_ability, _mage._status_manager.get_stats(), cooldown_manager)
-	
-	var aiming_strategy := SkeletonIceMageAimingStrategy.new(_mage._target_handler)
-	var target_context := AbilityAimingContext.new(aiming_strategy)
-	
-	var setup_strategy := SkeletonIceMageSetupStrategy.new(_mage.anim_tree)
-	var setup_context := AbilitySetupContext.new(setup_strategy)
-	
-	var execute_strategy := SkeletonIceMageExecuteStrategy.create(
+func _create_execute_strategy() -> AbilityExecuteStrategy:
+	return SkeletonIceMageExecuteStrategy.create(
 		_mage,
 		_mage.pivot,
 		_mage.staff_spawn_point,
-		null, # AI controller placeholder
+		null,
 	)
-	var execute_context := AbilityExecuteContext.new(execute_strategy)
-	
-	var recover_strategy := SkeletonIceMageRecoverStrategy.new(_mage.anim_tree)
-	var recover_context := AbilityRecoverContext.new(recover_strategy)
-	
-	var factory := AbilityExecutionFactory.new(target_context, setup_context, execute_context, recover_context)
-	var blackboard := AbilityExecutionBlackboard.new()
-	var execution := AbilityExecuter.new(blackboard, factory)
-	var manager := AbilityManager.new(execution)
-	
-	_mage._ability_system = AbilitySystem.new(registry, manager, cooldown_manager)
+
+func _create_attack_context() -> StateContextAttack:
+	return SkeletonIceMageAttack.new(_mage._ability_system)
