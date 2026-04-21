5dd19de21d38729dad9fae613cb41707ce032b7a
T Sch <tbs.schndlr95@gmail.com>
2026-04-21 11:27:36 +0200

    [refactoring] - unify CooldownManager and remove character specific methods

--- entities/shared/ability_system/cooldown_manager.gd
+++ entities/shared/ability_system/cooldown_manager.gd
@@ -2,8 +2,8 @@ class_name CooldownManager extends RefCounted
 
 signal cooldown_started(action: StringName, cooldown: float)
 
-# int = AbilityId, float = remaining cooldown
-var _cooldowns: Dictionary[int, float] = {}
+# StringName = AbilityId, float = remaining cooldown
+var _cooldowns: Dictionary[StringName, float] = {}
 
 func tick(delta: float) -> void:
 	for id in _cooldowns.keys():
@@ -11,15 +11,9 @@ func tick(delta: float) -> void:
 		if _cooldowns[id] <= 0:
 			_cooldowns.erase(id)
 
-func start_cooldown(id: int, cooldown: float) -> void:
+func start_cooldown(id: StringName, cooldown: float) -> void:
 	_cooldowns[id] = cooldown
+	cooldown_started.emit(id, cooldown)
 
-func start_character_cooldown(ability: AbilityData) -> void:
-	start_cooldown(ability.id, ability.cooldown)
-	cooldown_started.emit(ability.input, ability.cooldown)
-
-func has_cooldown(id: int) -> bool:
+func has_cooldown(id: StringName) -> bool:
 	return _cooldowns.has(id)
-
-func has_character_cooldown(ability: AbilityData) -> bool:
-	return _cooldowns.has(ability.id)
