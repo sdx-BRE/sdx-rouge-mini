a489da40a5f86f1882b72b311f30363ee04e61c2
T Sch <tbs.schndlr95@gmail.com>
2026-04-21 12:08:02 +0200

    [refactoring] - implement ability mapping and cooldown signal handling in MageSignals

--- entities/characters/mage/mage_signals.gd
+++ entities/characters/mage/mage_signals.gd
@@ -30,3 +30,14 @@ func _init(
 	casting_end = s_casting_end
 	casting_progressed = s_casting_progressed
 	skill_cooldown = s_skill_cooldown
+	
+var _ability_input_map: Dictionary[StringName, StringName] = {}
+
+func setup_ability_mapping(registry: AbilityRegistry) -> void:
+	_ability_input_map = registry.get_ability_input_map()
+
+func on_cooldown_started(id: StringName, cooldown: float) -> void:
+	print("test ", id)
+	if _ability_input_map.has(id):
+		var input_action := _ability_input_map[id]
+		skill_cooldown.emit(input_action, cooldown)
